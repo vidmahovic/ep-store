06047a099d3e912495508e6c42d1f8d39a8a54c5
vidmahovic <vid.mahovic@gmail.com>
2016-01-08 21:15:48 +0100

    Add HttpsRouting middleware to available route middlewares.

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -27,6 +27,7 @@ class Kernel extends HttpKernel
      */
     protected $routeMiddleware = [
         'auth' => \App\Http\Middleware\Authenticate::class,
+        'https' => \App\Http\Middleware\HttpsRouting::class,
         'auth.basic' => \Illuminate\Auth\Middleware\AuthenticateWithBasicAuth::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
     ];
