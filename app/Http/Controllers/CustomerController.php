aa7b85e68ce3a8dae4e60f1ed3a255a8041d7813
vidmahovic <vid.mahovic@gmail.com>
2016-01-12 16:26:18 +0100

    Modify customer controller access restrictions.

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,17 +2,22 @@
 
 namespace App\Http\Controllers;
 
+use App\Customer;
+use App\Http\Requests\StoreCustomerRequest;
+use App\User;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
+use Symfony\Component\HttpKernel\HttpCache\Store;
 
 class CustomerController extends Controller
 {
 
     public function __construct()
     {
-        $this->middleware('employee');
+        $this->middleware('employee', ['except' => ['show', 'edit', 'update']]);
     }
 
     /**
@@ -22,7 +27,9 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customers = Customer::with('user')->paginate(30);
+
+        return $customers;
     }
 
     /**
@@ -32,7 +39,7 @@ class CustomerController extends Controller
      */
     public function create()
     {
-        //
+        return 'Create view for storing a customer';
     }
 
     /**
@@ -41,31 +48,47 @@ class CustomerController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(StoreCustomerRequest $request)
     {
-        //
+        $user = Customer::create([
+            'phone' => $request->get('phone'),
+            'street' => $request->get('street'),
+            'city_id' => $request->get('city_id')
+        ]);
+
+        $customer = User::create([
+            'name' => $request->get('name'),
+            'surname' => $request->get('surname'),
+            'email' => $request->get('email'),
+            'password' => $request->get('password')
+        ]);
+
+        $customer->user()->save($user);
+
+        return redirect('/user')->with('customers', 'Stranka '.$user->name.' '.$user->surname.' je bila uspešno ustvarjena.');
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Customer $customer)
     {
-        //
+        dd($customer);
+        return $customer->with('user');
     }
 
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param Customer $customer
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Customer $customer)
     {
-        //
+        return $customer->with('user');
     }
 
     /**
@@ -89,5 +112,13 @@ class CustomerController extends Controller
     public function destroy($id)
     {
         //
+    }
+
+    public function deactivate($id) {
+
+    }
+
+    public function activate($id) {
+
     }
 }
