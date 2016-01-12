a2d0e87b4a15e1c2ace87ce96d5527e9c06b4ce6
vidmahovic <vid.mahovic@gmail.com>
2016-01-12 17:51:38 +0100

    Add middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends HttpKernel
      * @var array
      */
     protected $routeMiddleware = [
+        'customer' => \App\Http\Middleware\Customer::class,
         'admin' => \App\Http\Middleware\Admin::class,
         'employee' => \App\Http\Middleware\Employee::class,
         'auth' => \App\Http\Middleware\Authenticate::class,
