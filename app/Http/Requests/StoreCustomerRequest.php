94f6af0925c44e06c242e98a0fcd7493f28e927d
vidmahovic <vid.mahovic@gmail.com>
2016-01-12 16:22:58 +0100

    Create customer validation rules.

---
+++ app/Http/Requests/StoreCustomerRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Http\Requests\Request;
+use Illuminate\Support\Facades\Auth;
+
+class StoreCustomerRequest extends Request
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return ! Auth::user()->hasRole('admin');
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name' => 'reqired|alpha|max:255',
+            'surname' => 'required|alpha|255',
+            'street' => 'required|aplha_num|max:255',
+            'city_id' => 'required|exists:municipalities,id',
+            'phone' => 'string|max:255',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|confirmed|min:6',
+        ];
+    }
+}
