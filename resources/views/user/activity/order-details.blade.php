a5d0dac6758576381f9ab71d27c7daac1aef4a8b
espressyourself <erik.drobne@gmail.com>
2016-01-17 23:15:05 +0000

    order details template fix

--- resources/views/user/activity/order-details.blade.php
+++ resources/views/user/activity/order-details.blade.php
@@ -7,15 +7,15 @@
                     <th><strong>Ime produkta</strong></th>
                     <th><strong>Cena kos</strong></th>
                     <th><strong>Količina</strong></th>
-                    <th><strong>Cena</strong></th>
+                    <th><strong>Skupna cena</strong></th>
                 </tr>
                 </thead>
                 <tbody>
-                @foreach($order->products() as $product)
+                @foreach($order->products()->get() as $product)
                     <tr>
                         <td>{{ $product->name }}</td>
                         <td>{{ $product->price }}€</td>
-                        <td>{{ $product->quantity }}</td>
+                        <td>{{ $product->pivot->quantity }}</td>
                         <td>{{ number_format($product->quantity * $product->price, 2) }}€</td>
                     </tr>
                 @endforeach
