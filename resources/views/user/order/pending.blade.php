88ef761502118ce95c3b8935dbf88513c547e162
espressyourself <erik.drobne@gmail.com>
2016-01-20 14:45:52 +0000

    button position fix

--- resources/views/user/order/pending.blade.php
+++ resources/views/user/order/pending.blade.php
@@ -23,13 +23,17 @@
             <td>{{$order->created_at}}</td>
             <td><a href="{{ route('user.customers.show', [$order->ordered_by]) }}">{{$order->subscriber->user->name }} {{ $order->subscriber->user->surname }}</a></td>
             <td>
-                <div class="btn-group" role="group" aria-label="...">
-                    {!! Form::open(['url' => 'user/orders/'.$order->id, 'method' => 'PUT']) !!}
-                    <button type="submit" class="btn btn-success btn-xs">Potrdi</button>
-                    {!! Form::close() !!}
-                    {!! Form::open(['url' => 'user/orders/'.$order->id.'/deactivate', 'method' => 'PUT']) !!}
-                    <button type="submit" class="btn btn-warning btn-xs">Prekliči</button>
-                    {!! Form::close() !!}
+                <div class="row">
+                    <div class="col col-sm-6">
+                        {!! Form::open(['url' => 'user/orders/'.$order->id, 'method' => 'PUT']) !!}
+                        <button type="submit" class="btn btn-success btn-xs">Potrdi</button>
+                        {!! Form::close() !!}
+                    </div>
+                    <div class="col col-sm-6">
+                        {!! Form::open(['url' => 'user/orders/'.$order->id.'/deactivate', 'method' => 'PUT']) !!}
+                        <button type="submit" class="btn btn-warning btn-xs">Prekliči</button>
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </td>
         </tr>
